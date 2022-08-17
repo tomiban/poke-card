0cba66b57f3d377fbfbd6fb01a7dc03c65092893
tomiban <81537786+tomiban@users.noreply.github.com>
2022-08-17 14:46:17 -0300

    se realiza la funcion searchPokemon y renderPokemon

--- main.js
+++ main.js
@@ -1,38 +1,48 @@
-// const pokeCard = document.querySelector('[data-poke-card]');
-// const pokeName = document.querySelector('[data-poke-name]');
-// const pokeImg = document.querySelector('[data-poke-img-container]');
-// const pokeTypes = document.querySelector('[data-poke-types]');
-// const pokeId = document.querySelector('data-poke-types')
-// const pokeStats = document.querySelector('[data-poke-stats]');
+const pokeCard = document.querySelector('[data-poke-card]');
+const pokeName = document.querySelector('[data-poke-name]');
+const pokeImg = document.querySelector('[data-poke-img]');
+const pokeImgContainer = document.querySelector('[data-poke-img-container]');
+const pokeTypes = document.querySelector('[data-poke-types]');
+const pokeId = document.querySelector('[data-poke-types]')
+const pokeStats = document.querySelector('[data-poke-stats]');
 
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const pokemonContainer = document.querySelector('.pokemon-container');
 
-button.addEventListener('click', e => {
-    e.preventDefault();
-    traerPokemon(input.value); //trae el pokemon con el valor de lo que escribimos
-})
+const typeColors = { //diccionario de colores para mapear los tipos
+    electric: '#FFEA70',
+    normal: '#BO9398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#561D25',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#2F2F2F',
+    default: '#2A1A1F' 
+};
 
-function traerPokemon(pokemon) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`) //Realiza una request a la URL pasada y traer una respuesta
-    .then(res => res.json()) //convierta la respuesta en JSON 
-    .then(data => { //obtiene el JSON 
-        crearPokemon(data);
-    }) 
+const searchPokemon = event => {
+    event.preventDefault();
+    const {value} = event.target.pokemon
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
+        .then(data => data.json())
+        .then(res => renderPokemon(res)) 
 }
 
-function crearPokemon(pokemon) {
-    const img = document.createElement('img');
-    img.src = pokemon.sprites.front_default;
+const renderPokemon = data => {
+    console.log(data)
+    const sprite = data.sprites.front_default;
+    const {stats, types} = data; //desestructuracion de las propiedades con mismo nombre
+    
+    pokeName.textContent = data.name;
+    pokeId.textContent = `N° ${data.id}`;
+    pokeImg.setAttribute('src', sprite);
+    
 
-    const h3 = document.createElement('h3');
-    h3.textContent = pokemon.name;
-
-    const div = document.createElement('div');
-    div.appendChild(img);
-    div.appendChild(h3);
-    pokemonContainer.appendChild(div);
 }
-
-traerPokemon();
